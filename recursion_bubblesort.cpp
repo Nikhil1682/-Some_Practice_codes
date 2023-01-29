d7157851ae5ae4ac62b0752ec373be7e4cb2cc93
Nikhil Bakshi <110008146+Nikhil1682@users.noreply.github.com>
2023-01-29 16:49:33 +0530

    recursion_bubblesort

---
+++ recursion_bubblesort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std; 
+int sortarray(int arr[],int n){
+    if(n==0||n==1){
+        return 0;    //base case
+    }
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    sortarray(arr,n-1);
+
+
+}
+int main(){
+    int arr[6]={2,5,3,7,4,9};
+    sortarray(arr,6);
+    for(int i=0;i<6;i++){
+   cout<<arr[i]<<" ";
+    }
+    return 0;
+}
