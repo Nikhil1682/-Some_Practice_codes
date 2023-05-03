9e674b976beba0df19ac54fa81a9e92dfc71cb3e
Nikhil Bakshi <110008146+Nikhil1682@users.noreply.github.com>
2023-05-03 09:40:56 +0530

    fibonacciR

---
+++ fibonacciR.java
@@ -0,0 +1,20 @@
+public class fibonacciR {
+
+    public static void fib(int a,int b,int n){
+        if(n==0){
+            return;
+        }
+        int c=a+b;
+        System.out.print(c+ " ");
+        fib(b,c,n-1);
+    }
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=7;
+        System.out.print(a+ " ");
+        System.out.print(b+ " ");
+        fib(a,b,n-2);
+    }
+    
+}
