c8593999f189537f31bff647a90636b6f0bdeb75
Nikhil Bakshi <110008146+Nikhil1682@users.noreply.github.com>
2023-05-05 10:19:12 +0530

    CheckArraySorted

---
+++ CheckArraySorted.java
@@ -0,0 +1,19 @@
+public class CheckArraySorted {
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        
+        if(arr[i]<arr[i+1]){
+            return isSorted(arr,i+1);
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+       int arr[]={1,2,3,4};
+       System.out.println(isSorted(arr,0));;
+       
+    }
+}
